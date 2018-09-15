210b772761ab99b3027377af8d3746cfb405259d
mborges-pivotal <mborges@pivotal.io>
2018-09-14 22:57:40 -0500

    First commit

---
+++ src/main/java/com/borgescloud/movies/movieprocessor/MovieprocessorApplication.java
@@ -0,0 +1,12 @@
+package com.borgescloud.movies.movieprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieprocessorApplication.class, args);
+	}
+}
